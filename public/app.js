e25ffcd42473a6edfc9cf11a3601ef312bb14c40
ramp4 <rampatv13@gmail.com>
2020-02-09 20:57:56 +0300

    feat: implement put

--- public/app.js
+++ public/app.js
@@ -30,6 +30,22 @@ new Vue({
     },
     removeTodo(id) {
       this.todos = this.todos.filter(t => t.id !== id);
+    },
+    completeTodo(id) {
+      fetch("/api/todo/" + id, {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ done: true })
+      })
+        .then(res => res.json())
+        .then(({ todo }) => {
+          const idx = this.todos.findIndex(t => t.id === todo.id);
+          this.todos[idx].updatedAt = todo.updatedAt;
+        })
+        .catch(e => console.log(e));
+      console.log(id);
     }
   },
   filters: {
